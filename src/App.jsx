f2300517218205730fd61dfd75e320a9b9dcbd24
rivbmbh <rivaldoangelobomboah@gmail.com>
2025-07-07 21:06:24 +0800

    refactor: file name HeroSection from LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from "react";
 import Header from "./components/layout/Header";
-import LandingPage from "./components/layout/LandingPage/LandingPage";
+import LandingPage from "./components/layout/HeroSection";
 
 function App() {
   return (
