a995f80a33ba143f46101c151a5cee9086971204
rivbmbh <rivaldoangelobomboah@gmail.com>
2025-07-24 21:32:32 +0800

    feat: full view avatar at headerSection

--- src/components/layout/HeroSection/Avatar.jsx
+++ src/components/layout/HeroSection/Avatar.jsx
@@ -8,12 +8,15 @@ const Avatar = () => {
   }
   return (
     <>
+      {isView && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs bg-slate-950/60"></div>
+      )}
       <div
         className={`${
           isView
-            ? "w-full absolute scale-105 mx-auto"
+            ? "w-full max-w-full absolute scale-105 xs:scale-110 sm:scale-125 m-auto z-[55]"
             : "w-full mb-5 sm:mb-0 sm:w-1/2 h-max"
-        }`}
+        } transition-transform duration-200 ease-in-out`}
       >
         <img
           src="assets/img/rab.png"
@@ -21,6 +24,11 @@ const Avatar = () => {
           className="w-64 mx-auto"
           onClick={handleViewIMG}
         />
+        {isView && (
+          <p className="w-full mt-10 text-center">
+            * Click the image to exit full view.
+          </p>
+        )}
       </div>
     </>
   );
