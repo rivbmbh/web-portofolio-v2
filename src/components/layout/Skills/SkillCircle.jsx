c48d18fcd1e301fdba356eb50844d1caff747508
rivbmbh <rivaldoangelobomboah@gmail.com>
2025-07-28 14:45:58 +0800

    refactor: make modular with add component for circle tools

---
+++ src/components/layout/Skills/SkillCircle.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+
+const SkillCircle = ({
+  percent,
+  imgSrc,
+  strokeColor = "text-gray-700",
+  gridStartPosition = "",
+}) => {
+  const circleRef = useRef(null);
+  const offset = 100 - percent;
+
+  useEffect(() => {
+    gsap.fromTo(
+      circleRef.current,
+      { strokeDashoffset: 100 },
+      {
+        strokeDashoffset: offset,
+        duration: 2,
+        ease: "power2.out",
+      }
+    );
+  }, [offset]);
+
+  return (
+    <div
+      className={`relative flex items-center justify-center w-24 h-24 mx-auto ${gridStartPosition}`}
+    >
+      <svg
+        className="w-full h-full transform -rotate-90 text-slate-800"
+        viewBox="0 0 36 36"
+      >
+        <circle
+          className="bg-transparent"
+          stroke="currentColor"
+          strokeWidth="4"
+          fill="none"
+          cx="18"
+          cy="18"
+          r="15"
+        />
+        <circle
+          ref={circleRef}
+          className={strokeColor}
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          fill="none"
+          cx="18"
+          cy="18"
+          r="15"
+          strokeDasharray="100"
+          strokeDashoffset="100"
+        />
+      </svg>
+      <div className="absolute  w-[74%] h-[74%] overflow-hidden rounded-full">
+        <img src={imgSrc} alt="icon" className="object-contain" />
+      </div>
+    </div>
+  );
+};
+
+export default SkillCircle;
--- src/components/layout/Skills/Skills.jsx
+++ src/components/layout/Skills/Skills.jsx
@@ -1,27 +1,44 @@
-// import { Bar } from "react-chartjs-2";
+import SkillCircle from "./SkillCircle";
 
 const Skills = () => {
   return (
     <>
-      <section id="skills" className="w-full mt-12 text-white h-60">
-        <h2 className="text-5xl font-semibold text-center font-pixel">
-          My Skills
+      <section id="skills" className="w-full mt-12 mb-10 text-white font-pixel">
+        <h2 className="mb-10 text-5xl font-semibold text-center">
+          Web Developer Tools
         </h2>
-        <div className="flex flex-wrap justify-between w-4/5 mx-auto mt-5">
-          <div className="flex items-center justify-center w-16 h-16 overflow-hidden rounded-full">
-            <img
-              src="/assets/img/js.png"
-              alt="javascript"
-              className="object-contain w-full h-full"
-            />
-          </div>
-          <div className="flex items-center justify-center w-16 h-16 overflow-hidden bg-white rounded-full">
-            <img
-              src="/assets/img/php.png"
-              alt="javascript"
-              className="object-contain w-full h-full"
-            />
-          </div>
+
+        {/* circle tools/programming language */}
+        <div className="grid w-4/5 grid-cols-6 gap-2 mx-auto">
+          <SkillCircle percent={90} imgSrc="/assets/img/php.png" />
+          <SkillCircle
+            percent={75}
+            imgSrc="/assets/img/js.png"
+            gridStartPosition="col-start-3"
+          />
+          <SkillCircle
+            percent={100}
+            imgSrc="/assets/img/tailwindcss.png"
+            gridStartPosition="col-start-5"
+          />
+          <SkillCircle percent={85} imgSrc="/assets/img/ci.png" />
+          <SkillCircle percent={75} imgSrc="/assets/img/laravel.png" />
+          <SkillCircle
+            percent={70}
+            imgSrc="/assets/img/nodejs.png"
+            gridStartPosition="col-start-3"
+          />
+          <SkillCircle percent={60} imgSrc="/assets/img/react.png" />
+          <SkillCircle
+            percent={83}
+            imgSrc="/assets/img/mysql.png"
+            gridStartPosition="col-start-4"
+          />
+          <SkillCircle
+            percent={55}
+            imgSrc="/assets/img/mongodb.png"
+            gridStartPosition="col-start-6"
+          />
         </div>
       </section>
     </>
