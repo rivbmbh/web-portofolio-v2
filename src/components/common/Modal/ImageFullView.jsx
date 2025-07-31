5e443d44a89a9f16d643f1b8472c64ca7323705f
rivbmbh <rivaldoangelobomboah@gmail.com>
2025-07-31 09:04:25 +0800

    fix: bg blur at full view image

--- src/components/common/Modal/ImageFullView.jsx
+++ src/components/common/Modal/ImageFullView.jsx
@@ -8,7 +8,7 @@ export default function ImageFullView() {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex flex-col items-center justify-center h-screen p-8 text-white bg-black/70 font-pixel"
+      className="fixed inset-0 z-50 flex flex-col items-center justify-center h-screen p-8 text-white bg-slate-950/50 backdrop-blur-xs font-pixel"
       onClick={closeImage}
     >
       <img
