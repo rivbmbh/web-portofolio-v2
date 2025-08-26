bc6ee6ef14f4345d5c9170bbde3bddbcad33339d
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-08-26 21:15:51 +0800

    fix: about page

--- src/components/layout/About/About.jsx
+++ src/components/layout/About/About.jsx
@@ -1,15 +1,14 @@
 const About = () => {
   return (
     <>
-      <section id="about" className="px-8 mb-20 text-white py-14 font-pixel">
+      <section
+        id="about"
+        className="px-10 mb-24 text-white border border-gray-800 py-14 font-pixel"
+      >
         <div className="container mx-auto">
           <h2 className="mb-5 text-4xl font-bold text-center">About me</h2>
-          <p className="text-base text-center sm:px-5">
-            I someone who doesn't consider themselves exceptional but always
-            strives to improve and develop their skills, aspiring to become a
-            person with clear goals in life. I also enjoy playing games, but
-            it's mostly to pass the time, not much more. Additionally, you like
-            to go for rides on your motorcycle.
+          <p className="text-center uppercase text-8xl sm:px-5">
+            nothing special
           </p>
         </div>
       </section>
