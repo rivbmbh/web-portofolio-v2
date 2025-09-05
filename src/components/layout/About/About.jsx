495da4ec9292c8cb699b1e137145cf607bdaacd0
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-09-05 21:28:07 +0800

    fix: add text about me

--- src/components/layout/About/About.jsx
+++ src/components/layout/About/About.jsx
@@ -10,15 +10,23 @@ const About = () => {
               <h2 className="mb-5 text-3xl font-semibold text-center sm:text-4xl ">
                 About Me
               </h2>
-              <p className="text-base text-center sm:px-4">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Nostrum, delectus ut. Sequi ratione sit explicabo officia
-                expedita quaerat nesciunt dolore aliquid autem, rerum saepe
-                ipsum ut eum rem eligendi nisi provident porro officiis soluta?
-                Sapiente quas dolorem dolor nobis, quae ipsum iure soluta
-                consectetur eveniet voluptate placeat tempora, voluptatum
-                quisquam.
-              </p>
+              <div className="space-y-5">
+                <p className="text-base text-center sm:px-4">
+                  I originally pursued a degree in Information Systems, but
+                  along the way I discovered my true passion in programming.
+                  Since my fifth semester, I’ve been learning to code more
+                  seriously, mostly self-taught outside of campus, and my
+                  curiosity has only grown stronger. Over the past three years,
+                  I’ve explored various tech stacks, joined trainings, and built
+                  a deeper understanding of software development. For me, coding
+                  is not just a tool to solve business problems—it’s a space to
+                  create, experiment, and build something meaningful.
+                </p>
+                <p className="text-base text-center sm:px-4">
+                  As part of this learning journey, below I’ve included several
+                  certificates from trainings and programs I’ve completed.
+                </p>
+              </div>
             </div>
             <Certificate />
           </div>
