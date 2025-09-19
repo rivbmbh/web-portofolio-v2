df78fa443625ee63140105aea3005c916af97218
Rivaldo Bomboah <111403299+rivbmbh@users.noreply.github.com>
2025-09-19 09:38:52 +0800

    feat: add loading at resume

--- src/components/common/Modal/ViewResume.jsx
+++ src/components/common/Modal/ViewResume.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import ButtonCloseModal from "../Button/ButtonCloseModal";
+import LoadinDots from "../Effect/LoadingDots";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const ViewResume = ({ setIsOpenResume }) => {
   const [numPages, setNumPages] = useState();
   const [pageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   function onDocumentLoadSuccess({ numPages }) {
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
     setNumPages(numPages);
   }
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-xs bg-slate-950/60 font-pixel">
-      <div className="bg-transparent pb-5 pt-10 max-w-3xl w-[90%] h-[90vh] overflow-scroll snap-proximity rounded">
+      {loading && (
+        <LoadinDots className="absolute inset-x-0 text-lg font-bold text-center text-white" />
+      )}
+      <div
+        className={`bg-transparent pb-5 pt-10 max-w-3xl w-[90%] h-[90vh] overflow-scroll snap-proximity rounded transition-opacity duration-300 ${
+          loading ? "opacity-0" : "opacity-100"
+        }`}
+      >
         <Document
           file={`/assets/resume.pdf`}
           onLoadSuccess={onDocumentLoadSuccess}
@@ -25,8 +37,9 @@ const ViewResume = ({ setIsOpenResume }) => {
             renderTextLayer={false}
             pageNumber={pageNumber}
             width={735}
-            noData={<div className="">⚠️ Failed to load pdf file</div>}
-            loading={<div className="">⏳ Loading...</div>}
+            noData={
+              <div className="text-white">⚠️ Failed to load pdf file</div>
+            }
           />
           <ButtonCloseModal
             action={() => setIsOpenResume(false)}
